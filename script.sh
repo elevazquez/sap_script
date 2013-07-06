cebc61c9e5b70f427b758f1d27f2b91a1d731308
elena <mariaelena.vazquez@gmail.com>
2013-07-06 05:14:20 -0400

    correccion añadiir sudo

--- script.sh
+++ script.sh
@@ -9,10 +9,10 @@ echo "Bajando proyecto del repositorio..."
 	git clone git://github.com/elevazquez/sap_script.git
 echo "Fin de clonacion."
 echo "Creacion de Base de Datos..."
-	sudo chmod 777 -R $USER/proyecto_test
+	sudo chmod 777 -R /home/$USER/proyecto_test
 	cd /opt/PostgreSQL/9.1/bin
 	./psql -U postgres -W -c "CREATE DATABASE sap  WITH ENCODING='UTF8' ;"
-	cp /home/$USER/proyecto_test/sap_script/sap.sql /opt/PostgreSQL/9.1/bin/sap.sql
+	sudo cp /home/$USER/proyecto_test/sap_script/sap.sql /opt/PostgreSQL/9.1/bin/sap.sql
 	./psql -U postgres -d sap -f sap.sql
 echo "Fin de la Creacion de Base de Datos"
 echo "Levantando Proyecto...."
